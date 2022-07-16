eb241891e7b66e7db2e8bc18e30602d6865034ce
Cosmfulanito <91266300+Cosmfulanito@users.noreply.github.com>
2022-07-16 20:11:39 -0300

    add new tutorial

---
+++ tutorial-eslint/archivos/scrips.js
@@ -0,0 +1,21 @@
+
+
+
+const number = 2;
+	// eslint-disable-next-line no-console
+	console.log('number');
+
+const word = 'hola';
+
+
+window.addEventListener('scroll', () => {
+	// eslint-disable-next-line no-console
+	console.log('scroll');
+});
+
+const suma = number + 2;
+
+
+const h1 = document.getElementById('h1');
+
+h1.textContent = `${suma} ${word}`;
